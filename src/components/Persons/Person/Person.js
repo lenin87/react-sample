6c4dd5db13fdc94aaaeb0fc3e529a39348ad07a3
Lenin <lenin.komatipalli@gmail.com>
2019-04-16 19:02:26 +0530

    changed person.js file

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import Person from './Person.css'
-
-const person = (props) => {
+import React,{Component} from 'react';
+//  import Person from './Person.css'
+class Person extends Component {
+render(){
     return (
         <div className="Person">
-            <h4 onClick={props.click}>My name is {props.name} and I am  {props.age} years old !</h4>
-            <p >{props.children}</p>
-            <input type="text" value={props.name} onChange={props.chnaged} />
+            <h4 onClick={this.props.click}>My name is {this.props.name} and I am  {this.props.age} years old !</h4>
+            <p >{this.props.children}</p>
+            <input type="text" value={this.props.name} onChange={this.props.chnaged} />
         </div>
-    )
+    );
+        } 
 };
-
-
-export default person;
+export default Person;
